37f2a87d053b67d0fdf32b80c5bdf0e6956b0264
usman <muhammad.usman@funavy.com>
2023-02-27 16:35:18 +0500

    added one tap google auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgOneTapService } from 'ng-google-one-tap';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'social-login';
+  
+  userDetails:any;
+  constructor(private oneTap: NgOneTapService) { }
+
+   ngOnInit() {
+        /** Initialize OneTap, While initialing you can pass config  like this.oneTap.tapInitialize(config) here config is optional. **/
+        this.oneTap.tapInitialize();
+
+        /** Subscribe the Tap Moment. following response options all have self explanatory.
+         *  If you want more info please refer at bottom of the document attached link. **/
+        this.oneTap.promtMoment.subscribe(res => { 
+           res.getDismissedReason(); 
+           res.getMomentType();
+           res.getNotDisplayedReason();
+           res.getSkippedReason();
+           res.isDismissedMoment();
+           res.isDisplayed();
+           res.isNotDisplayed();
+           res.isSkippedMoment();
+        });
+
+        /** The JWT credentials will be returned as a response after completing the one tap process.  **/
+        this.oneTap.oneTapCredentialResponse.subscribe(res => {
+            /**  Response
+             * clientId: your client ID,
+             * client_id: your client ID,
+             * credential: The credential/secret key is utilized for user validation and information retrieval. Validation can be performed on the backend server/platform using the appropriate Google library. Please refer to the backend implementation details at the bottom of the document
+             **/
+        });
+
+        /** Authentication validation can be performed using the Google OAuth2 APIs, eliminating the need for client_id validation on the backend and the retrieval of user details **/
+        this.oneTap.authUserResponse.subscribe(res => { 
+            this.userDetails = res;
+        });
+
+   }
+
+    tapCancel() {
+        this.oneTap.cancelTheTap();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgGoogleOneTapModule } from 'ng-google-one-tap';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgGoogleOneTapModule.config(
+      {
+        client_id: '820987339217-j0in5e0ujrdtl4ht863909c5uktlialn.apps.googleusercontent.com',
+        scope:'https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.readonly&',
+        cancel_on_tap_outside: false,
+        authvalidate_by_googleapis: false,
+        auto_select: false,
+        disable_exponential_cooldowntime: false,
+        context: 'signup',
+      })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
